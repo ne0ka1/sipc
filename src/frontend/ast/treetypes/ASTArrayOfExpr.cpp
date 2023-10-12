10de7c30b2147234ceb10186d0a841624da0cfeb
Wesley Lewis <wjl4jj@virginia.edu>
2023-10-12 16:40:53 -0400

    created ArrayOfExpr to separate array expressions

---
+++ src/frontend/ast/treetypes/ASTArrayOfExpr.cpp
@@ -0,0 +1,40 @@
+#include "ASTArrayOfExpr.h"
+#include "ASTVisitor.h"
+#include "ASTinternal.h"
+// TODO: Fix Code and clean up code 
+ASTArrayOfExpr::ASTArrayOfExpr(
+    std::vector<std::unique_ptr<ASTExpr>> ELEMENTS) {
+  for (auto &element : ELEMENTS) {
+    std::shared_ptr<ASTExpr> e = std::move(ELEMENTS);
+    this->ELEMENTS.push_back(std::move(e));
+  }
+}
+
+std::vector<ASTExpr *> ASTArrayOfExpr::getElements() const {
+  return rawRefs(ELEMENTS);
+}
+
+void ASTArrayOfExpr::accept(ASTVisitor *visitor) {
+  if (visitor->visit(this)) {
+    for (auto &elem : getElements()) {
+      elem->accept(visitor);
+    }
+  }
+  visitor->endVisit(this);
+}
+
+std::ostream &ASTArrayOfExpr::print(std::ostream &out) const {
+  out << "[";
+  out << *ELEMENTS[0] << " of " << *ELEMENTS[1];
+  out << "]";
+  return out;
+}
+
+// Get children
+std::vector<std::shared_ptr<ASTNode>> ASTArrayOfExpr::getChildren() {
+  std::vector<std::shared_ptr<ASTNode>> children;
+  for (auto &elem : ELEMENTS) {
+    children.push_back(elem);
+  }
+  return children;
+}
---
+++ src/frontend/ast/treetypes/ASTArrayOfExpr.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ASTExpr.h"
+
+/*! \brief Class for array of statements i.e. [3 of 5]
+ */
+
+class ASTArrayOfExpr : public ASTExpr {
+  std::vector<std::shared_ptr<ASTExpr>> ELEMENTS = {};
+
+public:
+  ASTArrayOfExpr(std::vector<std::unique_ptr<ASTExpr>> ELEMENTS);
+  void accept(ASTVisitor *visitor) override;
+  std::vector<ASTExpr *> getElements() const;
+  std::vector<std::shared_ptr<ASTNode>> getChildren() override;
+  llvm::Value *codegen() override;
+
+protected:
+  std::ostream &print(std::ostream &out) const override;
+};
