fddbe2838bc736106f32b77f8ac858342f3d30a6
Wesley Jonah Lewis <wjl4jj@portal09.cs.Virginia.EDU>
2023-10-15 16:08:50 -0400

    fixed vector error

--- src/frontend/ast/treetypes/ASTArrayOfExpr.cpp
+++ src/frontend/ast/treetypes/ASTArrayOfExpr.cpp
@@ -1,11 +1,11 @@
 #include "ASTArrayOfExpr.h"
 #include "ASTVisitor.h"
 #include "ASTinternal.h"
-// TODO: Fix Code and clean up code 
+
 ASTArrayOfExpr::ASTArrayOfExpr(
     std::vector<std::shared_ptr<ASTExpr>> ELEMENTS) {
   for (auto &element : ELEMENTS) {
-    std::shared_ptr<ASTExpr> e = std::move(ELEMENTS);
+    std::shared_ptr<ASTExpr> e = std::move(element);
     this->ELEMENTS.push_back(std::move(e));
   }
 }
